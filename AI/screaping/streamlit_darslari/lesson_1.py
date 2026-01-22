a13c479488d184b993237805d41475a02aee6946
Suhrob <panjiyevsuhrob84@gmail.com>
2026-01-23 00:02:41 +0500

    yangi darslar o'zlashtirildi.

--- AI/screaping/streamlit_darslari/lesson_1.py
+++ AI/screaping/streamlit_darslari/lesson_1.py
@@ -1,6 +1,9 @@
 from inspect import markcoroutinefunction
 import json
 import streamlit as st
+import pandas as pd
+
+tabel=pd.DataFrame({"Column 1": [1,2,3,4,5,6,7], "Column 2": [11,12,13,14,15,16,17]})
 
 st.title("Salom! Men streamlit web app man.")
 st.subheader("Salom men subheader man.")
@@ -28,3 +31,36 @@ print("hello world")
 def funct():
     return 0; """
 st.code(code, language="python")
+
+st.write("## H2")
+
+st.metric(label="Wind Speed", value="120ms⁻¹", delta="1.4ms⁻¹")
+
+st.table(tabel)
+st.dataframe(tabel)
+
+# )))))))))))))))))))))))))))))))))))))))))))
+
+st.markdown("""
+ :red[IMDb top filmlar]
+""")
+
+code="""
+print("Salom dunyo!")
+def abd():
+    return 0;
+a+b=c
+bu yer string type, yani
+faqat malumot beradi.
+"""
+st.code(code, language="python")
+
+st.metric(label="salom",
+          value="alik",
+          delta="-25ms²"
+          )
+st.metric(label="Dushanba", value="Quyoshli", delta="20m/s²")
+
+st.image("foto.jpg", caption="Bu dasturchining rasmi")
+st.audio("xcho.mp3")
+st.video("video.mp4")
